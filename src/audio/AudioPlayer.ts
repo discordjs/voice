fbb3b9ab261b67c625ffd35f87136ce598d0cd1d
Amish Shah <amishshah.2k@gmail.com>
2021-02-04 11:36:32 +0000

    refactor(AudioPlayer): rename connections to subscribers

--- src/audio/AudioPlayer.ts
+++ src/audio/AudioPlayer.ts
@@ -87,7 +87,7 @@ export class AudioPlayer extends EventEmitter {
 	 * A list of VoiceConnections that are registered to this AudioPlayer. The player will attempt to play audio
 	 * to the streams in this list.
 	 */
-	private readonly connections: VoiceConnection[];
+	private readonly subscribers: VoiceConnection[];
 
 	/**
 	 * The behaviour that the player should follow when it enters certain situations.
@@ -106,7 +106,7 @@ export class AudioPlayer extends EventEmitter {
 	 */
 	public constructor(options: CreateAudioPlayerOptions) {
 		super();
-		this.connections = [];
+		this.subscribers = [];
 		this._state = {
 			status: AudioPlayerStatus.Idle
 		};
@@ -123,8 +123,8 @@ export class AudioPlayer extends EventEmitter {
 	 * @param connection The connection to play to
 	 */
 	public subscribe(connection: VoiceConnection) {
-		if (!this.connections.includes(connection)) {
-			this.connections.push(connection);
+		if (!this.subscribers.includes(connection)) {
+			this.subscribers.push(connection);
 		}
 	}
 
@@ -135,8 +135,8 @@ export class AudioPlayer extends EventEmitter {
 	 * If the connection is not in the play list, this method does nothing.
 	 */
 	public unsubscribe(connection: VoiceConnection) {
-		const index = this.connections.indexOf(connection);
-		if (index !== -1) this.connections.splice(index, 1);
+		const index = this.subscribers.indexOf(connection);
+		if (index !== -1) this.subscribers.splice(index, 1);
 	}
 
 	/**
@@ -280,7 +280,7 @@ export class AudioPlayer extends EventEmitter {
 		state.nextTime += FRAME_LENGTH;
 
 		// List of connections that can receive the packet
-		const playable = this.connections.filter(connection => connection.state.status === VoiceConnectionStatus.Ready);
+		const playable = this.subscribers.filter(connection => connection.state.status === VoiceConnectionStatus.Ready);
 
 		// Dispatch any audio packets that were prepared in the previous cycle
 		playable.forEach(connection => connection.dispatchAudio());
@@ -350,7 +350,7 @@ export class AudioPlayer extends EventEmitter {
 	 * they are no longer speaking. Called once playback of a resource ends.
 	 */
 	private _signalStopSpeaking() {
-		return this.connections.forEach(connection => connection.setSpeaking(false));
+		return this.subscribers.forEach(connection => connection.setSpeaking(false));
 	}
 
 	/**
@@ -360,7 +360,7 @@ export class AudioPlayer extends EventEmitter {
 	 * @param packet The Opus packet to be prepared by each receiver
 	 * @param receivers The connections that should play this packet
 	 */
-	private _preparePacket(packet: Buffer, receivers = this.connections) {
+	private _preparePacket(packet: Buffer, receivers = this.subscribers) {
 		receivers.forEach(connection => connection.prepareAudioPacket(packet));
 	}
 }
