1ef70dc10554b6e923d1bcf956853d0b85d3d8bc
Amish Shah <amishshah.2k@gmail.com>
2021-02-05 10:47:50 +0000

    feat: export all public types (#24)
    * refactor: export all public types
    
    * feat: export StreamType from TransformerGraph
    
    * refactor: remove unused import

---
+++ src/audio/index.ts
@@ -0,0 +1,15 @@
+export {
+	AudioPlayer,
+	AudioPlayerStatus,
+	NoSubscriberBehaviour,
+	createAudioPlayer
+} from './AudioPlayer';
+
+export {
+	AudioResource,
+	createAudioResource
+} from './AudioResource';
+
+export { PlayerSubscription } from './PlayerSubscription';
+
+export { StreamType } from './TransformerGraph';
---
+++ src/joinVoiceChannel.ts
@@ -0,0 +1,31 @@
+import { VoiceChannel } from 'discord.js';
+import { createVoiceConnection } from './VoiceConnection';
+import { JoinConfig } from './DataStore';
+
+/**
+ * The options that can be given when joining a voice channel
+ */
+export interface JoinVoiceChannelOptions {
+	/**
+	 * If true, debug messages will be enabled for the voice connection and its
+	 * related components. Defaults to false.
+	 */
+	debug?: boolean;
+}
+
+/**
+ * Creates a VoiceConnection to a Discord.js Voice Channel.
+ *
+ * @param voiceChannel the voice channel to connect to
+ * @param options the options for joining the voice channel
+ */
+export function joinVoiceChannel(voiceChannel: VoiceChannel, options?: JoinVoiceChannelOptions) {
+	const joinConfig: JoinConfig = {
+		channelId: voiceChannel.id,
+		guild: voiceChannel.guild,
+		selfDeaf: true,
+		selfMute: false
+	};
+
+	return createVoiceConnection(joinConfig, { debug: false, ...options });
+}
