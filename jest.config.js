e7e209f71f19befe4a585bd9371f2344b2f53110
Amish Shah <amishshah.2k@gmail.com>
2021-03-03 14:51:27 +0000

    chore: remove coverage targets temporarily

--- jest.config.js
+++ jest.config.js
@@ -8,13 +8,5 @@ module.exports = {
 	],
 	coverageProvider: 'v8',
 	coverageDirectory: 'coverage',
-	coverageReporters: ['text', 'lcov', 'clover'],
-	coverageThreshold: {
-		global: {
-			branches: 80,
-			functions: 80,
-			lines: 80,
-			statements: 80,
-		},
-	}
+	coverageReporters: ['text', 'lcov', 'clover']
 };
