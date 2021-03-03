4e035479bc5aa5d938bf546cda8451bfd9eabcb1
Amish Shah <amishshah.2k@gmail.com>
2021-03-03 16:40:55 +0000

    Update src/audio/AudioResource.ts
    Co-authored-by: Papaia <43409674+papaia@users.noreply.github.com>

--- src/audio/AudioResource.ts
+++ src/audio/AudioResource.ts
@@ -16,7 +16,7 @@ interface CreateAudioResourceOptions<T> {
 	/**
 	 * Optional metadata that can be attached to the resource (e.g. track title, random ID).
 	 * This is useful for identification purposes when the resource is passed around in events.
-	 * @see AudioResource#metadata
+	 * See {@link AudioResource.metadata}
 	 */
 	metadata?: T;
 
