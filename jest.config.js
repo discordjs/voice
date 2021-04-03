7b92f96cbb589c20b98142ad3cf003bc32ebcf7c
Amish Shah <amishshah.2k@gmail.com>
2021-04-03 16:35:53 +0100

    chore: fix coverage rule

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
 	testEnvironment: 'node',
 	collectCoverage: true,
-	collectCoverageFrom: ['src/**/*.ts', '!src/**/__tests__/*.ts'],
+	collectCoverageFrom: ['src/**/*.ts', '!src/**/__tests__/**/*.ts'],
 	coverageDirectory: 'coverage',
 	coverageReporters: ['text', 'lcov', 'clover'],
 };
