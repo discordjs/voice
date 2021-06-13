9cd36179bd2e9b7edd2948e7b5ad52242cedb824
Amish Shah <amishshah.2k@gmail.com>
2021-06-13 14:53:53 +0100

    fix(Example): improve reconnection logic

--- examples/music-bot/src/music/subscription.ts
+++ examples/music-bot/src/music/subscription.ts
@@ -5,9 +5,14 @@ import {
 	createAudioPlayer,
 	entersState,
 	VoiceConnection,
+	VoiceConnectionDisconnectReason,
 	VoiceConnectionStatus,
 } from '@discordjs/voice';
 import { Track } from './track';
+
+function wait(time: number) {
+	return new Promise((resolve) => setTimeout(resolve, time).unref());
+}
 
 /**
  * A MusicSubscription exists for each active VoiceConnection. Each subscription has its own audio player and queue,
@@ -18,41 +23,66 @@ export class MusicSubscription {
 	public readonly audioPlayer: AudioPlayer;
 	public queue: Track[];
 	public queueLock = false;
-	public connectPromise?: Promise<void>;
+	public readyLock = false;
 
 	public constructor(voiceConnection: VoiceConnection) {
 		this.voiceConnection = voiceConnection;
 		this.audioPlayer = createAudioPlayer();
 		this.queue = [];
 
-		// Configure voice connection
-		this.voiceConnection.on('stateChange', (_, newState) => {
+		this.voiceConnection.on('stateChange', async (_, newState) => {
 			if (newState.status === VoiceConnectionStatus.Disconnected) {
-				// In the Disconnected state, try to reconnect or destroy if too many failed attempts
-				if (this.voiceConnection.reconnectAttempts < 5) {
-					setTimeout(() => {
-						if (this.voiceConnection.state.status === VoiceConnectionStatus.Disconnected) {
-							this.voiceConnection.reconnect();
-						}
-					}, (this.voiceConnection.reconnectAttempts + 1) * 5_000).unref();
+				if (newState.reason === VoiceConnectionDisconnectReason.WebSocketClose && newState.closeCode === 4014) {
+					/*
+						If the WebSocket closed with a 4014 code, this means that we should not manually attempt to reconnect,
+						but there is a chance the connection will recover itself if the reason of the disconnect was due to
+						switching voice channels. This is also the same code for the bot being kicked from the voice channel,
+						so we allow 5 seconds to figure out which scenario it is. If the bot has been kicked, we should destroy
+						the voice connection.
+					*/
+					try {
+						await entersState(this.voiceConnection, VoiceConnectionStatus.Connecting, 5_000);
+						// Probably moved voice channel
+					} catch {
+						this.voiceConnection.destroy();
+						// Probably removed from voice channel
+					}
+				} else if (this.voiceConnection.reconnectAttempts < 5) {
+					/*
+						The disconnect in this case is recoverable, and we also have <5 repeated attempts so we will reconnect.
+					*/
+					await wait((this.voiceConnection.reconnectAttempts + 1) * 5_000);
+					if (this.voiceConnection.state.status === VoiceConnectionStatus.Disconnected) {
+						this.voiceConnection.reconnect();
+					}
 				} else {
+					/*
+						The disconnect in this case may be recoverable, but we have no more remaining attempts - destroy.
+					*/
 					this.voiceConnection.destroy();
 				}
 			} else if (newState.status === VoiceConnectionStatus.Destroyed) {
-				// In the Destroyed state, stop the player and kill the queue
+				/*
+					Once destroyed, stop the subscription
+				*/
 				this.stop();
 			} else if (
-				!this.connectPromise &&
+				!this.readyLock &&
 				(newState.status === VoiceConnectionStatus.Connecting || newState.status === VoiceConnectionStatus.Signalling)
 			) {
-				// In the Signalling or Connecting states, set a time limit to prevent starvation
-				// These states can be entered after Ready in an automatic reconnect, e.g. unknown close code, UDP keep-alive failed
-				this.connectPromise = entersState(this.voiceConnection, VoiceConnectionStatus.Ready, 20_000)
-					.then(() => undefined)
-					.catch(() => {
-						if (this.voiceConnection.state.status !== VoiceConnectionStatus.Destroyed) this.voiceConnection.destroy();
-					})
-					.finally(() => (this.connectPromise = undefined));
+				/*
+					In the Signalling or Connecting states, we set a 20 second time limit for the connection to become ready
+					before destroying the voice connection. This stops the voice connection permanently existing in one of these
+					states.
+				*/
+				this.readyLock = true;
+				try {
+					await entersState(this.voiceConnection, VoiceConnectionStatus.Ready, 20_000);
+				} catch {
+					if (this.voiceConnection.state.status !== VoiceConnectionStatus.Destroyed) this.voiceConnection.destroy();
+				} finally {
+					this.readyLock = false;
+				}
 			}
 		});
 
@@ -90,7 +120,7 @@ export class MusicSubscription {
 	public stop() {
 		this.queueLock = true;
 		this.queue = [];
-		this.audioPlayer.stop();
+		this.audioPlayer.stop(true);
 	}
 
 	/**
