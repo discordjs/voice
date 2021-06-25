b53d33b75e27e8539a2283c557b04b40cc4c7841
Amish Shah <amish@shah.gg>
2021-06-25 17:00:26 +0100

    fix(VoiceConnection): disconnect method

--- src/VoiceConnection.ts
+++ src/VoiceConnection.ts
@@ -499,6 +499,7 @@ export class VoiceConnection extends TypedEmitter<VoiceConnectionEvents> {
 		) {
 			return false;
 		}
+		Object.assign(this.joinConfig, { channelId: null });
 		if (!this.state.adapter.sendPayload(createJoinVoiceChannelPayload(this.joinConfig))) {
 			this.state = {
 				adapter: this.state.adapter,
