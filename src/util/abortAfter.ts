945cdb492915345885051416437ce4cfb61a7bce
Amish Shah <amishshah.2k@gmail.com>
2021-08-08 12:42:53 +0100

    refactor(Util): use AbortSignal in entersState

---
+++ src/util/abortAfter.ts
@@ -0,0 +1,10 @@
+/**
+ * Creates an abort controller that aborts after the given time.
+ * @param delay - The time in milliseconds to wait before aborting
+ */
+export function abortAfter(delay: number): AbortController {
+	const ac = new AbortController();
+	const timeout = setTimeout(() => ac.abort(), delay);
+	ac.signal.addEventListener('abort', () => clearTimeout(timeout));
+	return ac;
+}
