7950bfc20dbf5c9572148efd06d5b118d8a99e28
oceanroleplay <indianoceanroleplay@gmail.com>
2021-11-10 18:20:18 +0530

    chore: fixes #227

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,6 +1,9 @@
 import type { Options } from 'tsup';
 
 export const tsup: Options = {
+	banner: {
+		js: "import { createRequire as topLevelCreateRequire } from 'module';\n const require = topLevelCreateRequire(import.meta.url);",
+	},
 	clean: true,
 	dts: true,
 	entryPoints: ['src/index.ts'],
