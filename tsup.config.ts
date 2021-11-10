148cf0163d0e24a8819e74c7961743ea61bde3ef
oceanroleplay <indianoceanroleplay@gmail.com>
2021-11-10 18:36:23 +0530

    chore: fix build issue

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import type { Options } from 'tsup';
 
 export const tsup: Options = {
 	banner: {
-		js: "import { createRequire as topLevelCreateRequire } from 'module';\n const require = topLevelCreateRequire(import.meta.url);",
+		js: 'import { createRequire as topLevelCreateRequire } from "module";\n const require = topLevelCreateRequire(import.meta.url);',
 	},
 	clean: true,
 	dts: true,
