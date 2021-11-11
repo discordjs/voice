616f2bcfde47e55ac7b09f4faaa07f15d78c11a5
Indian Ocean Roleplay <indianoceanroleplay@gmail.com>
2021-11-11 11:25:49 +0530

    fix(esm): resolve esm imports (#229)

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,6 +1,9 @@
 import type { Options } from 'tsup';
 
 export const tsup: Options = {
+	banner: {
+		js: 'import path from "path";\nimport { createRequire as topLevelCreateRequire } from "module";\nconst require = topLevelCreateRequire(path.resolve(import.meta.url));\n',
+	},
 	clean: true,
 	dts: true,
 	entryPoints: ['src/index.ts'],
