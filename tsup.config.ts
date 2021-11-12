644af9579f02724c489514f482640b8413d2c305
iCrawl <buechler.noel@outlook.com>
2021-11-12 10:52:50 +0100

    fix: postbuild script

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,17 +1,12 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig((options) => {
-	return {
-		banner: {
-			js:
-				options.format[0] === 'esm'
-					? 'import{resolve as requireResolve}from"path";import{createRequire as topLevelCreateRequire}from"module";const require=topLevelCreateRequire(requireResolve(""));'
-					: '',
-		},
-		entryPoints: ['src/index.ts'],
-		minify: true,
-		skipNodeModulesBundle: true,
-		sourcemap: true,
-		target: 'es2021',
-	};
+export default defineConfig({
+	clean: true,
+	dts: true,
+	entryPoints: ['src/index.ts'],
+	format: ['esm', 'cjs'],
+	minify: true,
+	skipNodeModulesBundle: true,
+	sourcemap: true,
+	target: 'es2021',
 });
